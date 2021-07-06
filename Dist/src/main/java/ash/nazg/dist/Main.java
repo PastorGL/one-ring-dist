45987ff017279c5b79f5a4bd814eec319a076581
Alexey Evdokimov <a.evdokimov@russianit.ru>
2021-07-06 19:45:25 +0400

    LOC-197 case with un-rewritten paths in non-local Dist

--- Dist/src/main/java/ash/nazg/dist/Main.java
+++ Dist/src/main/java/ash/nazg/dist/Main.java
@@ -22,6 +22,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple3;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
@@ -69,36 +70,46 @@ public class Main {
             }
             LayerResolver distResolver = new LayerResolver(props);
 
-            String inputPath;
+            String inputPath = null;
+            String outputPath = null;
+
+            Direction distDirection = Direction.parse(configBuilder.getOptionValue("d"));
+            if (!local) {
+                if (distDirection.toCluster) {
+                    inputPath = config.getForeignValue(Constants.INPUT_LAYER + "." + Constants.PATH);
+                    if (config.getForeignValue(Constants.INPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS) == null) {
+                        config.setForeignLayer(Constants.INPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS, inputPath);
+                    }
+                    inputPath = null;
+                    config.setForeignLayer(Constants.INPUT_LAYER + "." + Constants.PATH, inputPath);
+                }
+                if (distDirection.fromCluster) {
+                    outputPath = config.getForeignValue(Constants.OUTPUT_LAYER + "." + Constants.PATH);
+                    if (config.getForeignValue(Constants.OUTPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS) == null) {
+                        config.setForeignLayer(Constants.OUTPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS, outputPath);
+                    }
+                    outputPath = null;
+                    config.setForeignLayer(Constants.OUTPUT_LAYER + "." + Constants.PATH, outputPath);
+                }
+            }
+
             if (configBuilder.hasOption("i")) {
                 inputPath = configBuilder.getOptionValue("i");
-            } else {
-                inputPath = config.getForeignValue(Constants.INPUT_LAYER + "." + Constants.PATH);
             }
             if (inputPath == null) {
                 inputPath = local ? "." : "hdfs:///input";
                 config.setForeignLayer(Constants.INPUT_LAYER + "." + Constants.PATH, inputPath);
             }
-            if (config.getForeignValue(Constants.INPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS) == null) {
-                config.setForeignLayer(Constants.INPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS, inputPath);
-            }
 
-            String outputPath;
             if (configBuilder.hasOption("o")) {
                 outputPath = configBuilder.getOptionValue("o");
-            } else {
-                outputPath = config.getForeignValue(Constants.OUTPUT_LAYER + "." + Constants.PATH);
             }
             if (outputPath == null) {
                 outputPath = local ? "." : "hdfs:///output";
                 config.setForeignLayer(Constants.OUTPUT_LAYER + "." + Constants.PATH, outputPath);
             }
-            if (config.getForeignValue(Constants.OUTPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS) == null) {
-                config.setForeignLayer(Constants.OUTPUT_LAYER + "." + Constants.PATH_PREFIX + Constants.DEFAULT_DS, outputPath);
-            }
 
             Direction taskDirection = Direction.parse(distResolver.get("wrap", "nop"));
-            Direction distDirection = Direction.parse(configBuilder.getOptionValue("d"));
             if (taskDirection.anyDirection && distDirection.anyDirection) {
                 InOutResolver ioResolver = new InOutResolver(config);
 
